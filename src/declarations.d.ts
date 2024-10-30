659fb67a45fa663ef67c919d1ca61103c4e42849
Matheus Franco <cajondefox@gmail.com>
2024-10-30 12:05:53 -0300

    feat: add fullscreen hook

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,3 +2,8 @@ declare module '*.jfif' {
     const value: string;
     export default value;
 }
+
+declare module '*.less' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
